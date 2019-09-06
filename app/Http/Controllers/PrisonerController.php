21cf9eaf7bb8b9b7ecdf284a1e624d211ef78a64
segunemma2003 <segunemma2003@gmail.com>
2019-09-06 15:04:07 +0100

    mine

--- app/Http/Controllers/PrisonerController.php
+++ app/Http/Controllers/PrisonerController.php
@@ -75,7 +75,7 @@ class PrisonerController extends Controller
             $result=$this->face($facess);   
             // dd($result->status()->description());
             if($result->status()->description()=="Ok"){
-                $image=$result->get();
+                $image=$result->get()[0]->id;
                 dd($image);
             }
         }
