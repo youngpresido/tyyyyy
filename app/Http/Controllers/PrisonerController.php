4be07be019c1a552694d17c792e19a8919bcc060
segunemma2003 <segunemma2003@gmail.com>
2019-09-06 14:36:43 +0100

    mine

--- app/Http/Controllers/PrisonerController.php
+++ app/Http/Controllers/PrisonerController.php
@@ -73,7 +73,7 @@ class PrisonerController extends Controller
             $facess=env('APP_URL')."{$filePath}";
             // dd($facess);
             $result=$this->face($facess);   
-            // dd($result);
+            dd($result->status()->description());
             if($result->status()->description()=="ok"){
                 $image=$result->deserialized()[0]['id'];
                 dd($image);
