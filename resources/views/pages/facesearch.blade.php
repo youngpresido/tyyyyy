91ce3519678b950f374ae407da79a76a682702a0
segunemma2003 <segunemma2003@gmail.com>
2019-09-06 15:55:01 +0100

    mine

--- resources/views/pages/facesearch.blade.php
+++ resources/views/pages/facesearch.blade.php
@@ -1,8 +1,18 @@
 @extends('partials.master')
 @section('content')
+<div id="wrapper">
+
+    <!-- Sidebar -->
+   @include('partials.nav')
+   <div id="content-wrapper">
+
+<div class="container-fluid">
 <form method="post" enctype="multipart/form-data">
     @csrf
     <input type="file" name="image">
     <input type="submit">
 </form>
+</div>
+   </div>
+</div>
 @endsection
