d0f98bafeac3b78b40d0331ef74393b0e64272e3
segunemma2003 <segunemma2003@gmail.com>
2019-09-07 00:33:57 +0100

    ete

--- app/Traits/UploadTrait.php
+++ app/Traits/UploadTrait.php
@@ -9,9 +9,9 @@ trait UploadTrait
     public function uploadOne(UploadedFile $uploadedFile, $folder = null, $disk = 'public', $filename = null)
     {
         $name = !is_null($filename) ? $filename : str_random(25);
-        dd($uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk));
+        // dd($uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk));
         $file = $uploadedFile->storeAs($folder, $name.'.'.$uploadedFile->getClientOriginalExtension(), $disk);
-        dd($file);
+        // dd($file);
 
         return $file;
     }
