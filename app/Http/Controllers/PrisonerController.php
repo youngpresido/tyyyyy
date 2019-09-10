80464cfca4215529bad1261758d0a7ca77558606
segunemma2003 <segunemma2003@gmail.com>
2019-09-10 01:52:27 +0100

    ttt

--- app/Http/Controllers/PrisonerController.php
+++ app/Http/Controllers/PrisonerController.php
@@ -393,36 +393,19 @@ if ($err) {
         }
         public function detectface($url)
         {
-            // A sample PHP Script to POST data using cURL
-// Data in JSON format
- 
-$data = array(
-    "api_key"=>"md-UGvEKzPVwzYNwWkGXMMKRZoVl5dVc",
+            $data = array(
+                "api_key"=>"md-UGvEKzPVwzYNwWkGXMMKRZoVl5dVc",
                 "api_secret"=>"i6aysWk3H2h6LNgAqJhhar7SBEjUz7NC",
                 "image_url" => $url,
 );
- 
-$payload = json_encode($data);
- 
-// Prepare new cURL resource
-$ch = curl_init('https://api-us.faceplusplus.com/facepp/v3/detect');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLINFO_HEADER_OUT, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
- 
-// Set HTTP Header for POST request 
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Content-Length: ' . strlen($payload))
-);
- 
-// Submit the POST request
-$result = curl_exec($ch);
- dd($result);
-// Close cURL session handle
-curl_close($ch);
+            
+                        
+            $client = new Client();
+$response = $client->post('https://api-us.faceplusplus.com/facepp/v3/detect', ['body' => $data]);
 
+$result = $response->getBody()->getContents();
+dd($result);
+            
 
         }
 
