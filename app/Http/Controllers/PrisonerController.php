b43a1fb70556b18430f7056811b0da2c3218c276
segunemma2003 <segunemma2003@gmail.com>
2019-09-10 01:44:03 +0100

    ttt

--- app/Http/Controllers/PrisonerController.php
+++ app/Http/Controllers/PrisonerController.php
@@ -393,30 +393,35 @@ if ($err) {
         }
         public function detectface($url)
         {
-            $postdata = http_build_query(
-                array(
-                    "api_key"=>"md-UGvEKzPVwzYNwWkGXMMKRZoVl5dVc",
+            // A sample PHP Script to POST data using cURL
+// Data in JSON format
+ 
+$data = array(
+    "api_key"=>"md-UGvEKzPVwzYNwWkGXMMKRZoVl5dVc",
                 "api_secret"=>"i6aysWk3H2h6LNgAqJhhar7SBEjUz7NC",
                 "image_url" => $url,
-                )
-            );
-         
-            // Set the POST options
-            $opts = array('http' => 
-                array (
-                    'method' => 'POST',
-                    'header' => 'Content-type: application/xwww-form-urlencoded',
-                    'content' => $postdata
-                )
-            );
-         
-            // Create the POST context
-            $context  = stream_context_create($opts);
-         
-            // POST the data to an api
-            $url = 'https://api-us.faceplusplus.com/facepp/v3/detect';
-            $result = file_get_contents($url, false, $context);
-            dd($result);
+);
+ 
+$payload = json_encode($data);
+ 
+// Prepare new cURL resource
+$ch = curl_init('https://api-us.faceplusplus.com/facepp/v3/detect');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+ 
+// Set HTTP Header for POST request 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($payload))
+);
+ 
+// Submit the POST request
+$result = curl_exec($ch);
+ dd($result);
+// Close cURL session handle
+curl_close($ch);
 
 
         }
