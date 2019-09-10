bd98e979230f32e3c02b697fd7c82a40fbb706c2
segunemma2003 <segunemma2003@gmail.com>
2019-09-10 12:58:14 +0100

    segun

--- resources/views/pages/prisoners.blade.php
+++ resources/views/pages/prisoners.blade.php
@@ -21,7 +21,7 @@
         <thead>
             <tr>
             <th>id</th> 
-            <th>Image</th>
+        
         <th>SOC number</th>
 
 
@@ -31,9 +31,9 @@
         <th>State</th>
         <th>Marital Status</th>
         <th>Sex</th>
+        <th>Ethnicity</th>
         <th>Date of Birth</th>
          <th>Personnel</th>
-         
          <th>Actions</th>
             </tr>
         </thead>
@@ -47,29 +47,13 @@
          @foreach($prisoners as $prisoner)
         <tr>
             <td>{{$loop->index+1}}</td>
-            <td>
-            <?php
-            $t=env('APP_URL');
-            
-            ?>
-          
-            <img src="{{$t.$prisoner->image}}" style="width:50px; height:50px;" class="img-circle"/>
-            </td>
             <td>{{$prisoner->soc_number}}</td>
             <td>{{$prisoner->first_name}}</td>
             <td>{{$prisoner->last_name}}</td>
             <td>{{$prisoner->state}}</td>
             <td>{{$prisoner->marital_status}}</td>
             <td>{{$prisoner->sex}}</td>
-            <td>
-               @if($prisoner->ethnicity=="1")
-                  <td>Hausa</td>
-               @elseif($prisoner->ethnicity=="2")
-               <td>Yoruba</td>
-               @elseif($prisoner->ethnicity=="3")
-               <td>Igbo</td>
-               @endif
-            </td>
+            <td>{{$prisoner->ethnicity}}</td>
             <td>{{$prisoner->date_of_birth}}</td>
             <td>{{$prisoner->personnel}}</td>
             <td>
@@ -77,17 +61,15 @@
            
            <span class="glyphicon glyphicon-message"></span> View Details
 </a>   
-
-            <!-- <a href="" class="btn btn-info">
+            <a href="" class="btn btn-info">
             
             <span class="glyphicon glyphicon-edit"></span> Edit
 </a>
-        <a href="" class=" btn btn-danger">
+        <!-- <a href="" class=" btn btn-danger">
            
             <span class="glyphicon glyphicon-trash"></span> Delete
-</a>
- -->
- </td>
+</a> -->
+</td>
         </tr>
       @endforeach
    @endif
