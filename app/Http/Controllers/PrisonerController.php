169633d259252ed810af268eefa31c8c82d03d87
segunemma2003 <segunemma2003@gmail.com>
2019-09-10 02:43:36 +0100

    ttt

--- app/Http/Controllers/PrisonerController.php
+++ app/Http/Controllers/PrisonerController.php
@@ -461,7 +461,7 @@ $result = $response->getBody();
 // }
 // echo "\n\n";
 $myResult=json_decode((string) $result, true);
-// dd($myResult);
+dd($myResult);
 if($myResult['results'][0]['face_token']){
     return $myResult['results'][0]['face_token'];
 }else{
