cf4e196f061278e3e78dd28fbc50f2f8088ff458
segunemma2003 <segunemma2003@gmail.com>
2019-09-12 08:40:12 +0100

    as

--- config/flexcodesdk.php
+++ config/flexcodesdk.php
@@ -8,11 +8,11 @@ return [
     'devices' => array(
         // add device here
         array(
-            'name' 	=> env('FLEXCODE_DEVICE',"mydevice"),
-            'sn' 	=> env('FLEXCODE_SN',"AZ00T069431"),
-            'vc'	=> env('FLEXCODE_VC',"C10541FAAB7D615"),
-            'ac' 	=> env('FLEXCODE_AC',"ARBB08A8548BC28F9DD18YHJ"),
-            'vkey' 	=> env('FLEXCODE_VKEY',"8CD068380E2EF823BE718FB2C38916AF"),
+            'name' 	=> env('FLEXCODE_DEVICE'),
+            'sn' 	=> env('FLEXCODE_SN'),
+            'vc'	=> env('FLEXCODE_VC'),
+            'ac' 	=> env('FLEXCODE_AC'),
+            'vkey' 	=> env('FLEXCODE_VKEY'),
         ),
     ),
 ];
